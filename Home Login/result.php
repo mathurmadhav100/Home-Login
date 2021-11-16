571586e24a505e18646b382ffdcf8a3ee0b50cb5
mathurmadhav100 <72166575+mathurmadhav100@users.noreply.github.com>
2021-11-16 15:43:27 +0530

    Add files via upload

---
+++ Home Login/result.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<body>
+	<?php 
+		$name = $_POST[name];
+		$uname = $_POST[uname];
+		$psd = $_POST[password];
+		$phno = $_POST[phno];
+		echo"Your data has been successfully stores"
+	?>
+</body>
+</html>
